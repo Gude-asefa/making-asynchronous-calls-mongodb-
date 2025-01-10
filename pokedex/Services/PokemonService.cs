f23caf87f5b4c9d125f60e08f4de1541a6ebe74a
GUDINA ASEFA <gudinaasefa19@gmail.com>
2025-01-10 22:42:55 +0300

    final touch

--- pokedex/Services/PokemonService.cs
+++ pokedex/Services/PokemonService.cs
@@ -14,12 +14,13 @@ namespace pokedex.Services
             var mongoDatabase = mongoClient.GetDatabase(mongoDBSettings.Value.DatabaseName);
             _pokemonCollection = mongoDatabase.GetCollection<Pokemon>(mongoDBSettings.Value.PokemonCollectionName);
         }
+        // Method to retrieve all Pokémon from the collection
 
         public async Task<List<Pokemon>> GetPokemons()
         {
             return await _pokemonCollection.Find(_ => true).ToListAsync();
         }
-
+  // Method to retrieve a Pokémon by its ID
         public async Task<Pokemon> GetPokemonById(string id)
         {
             var pokemon = await _pokemonCollection.Find(p => p.Id == id).FirstOrDefaultAsync();
@@ -27,7 +28,7 @@ namespace pokedex.Services
                 throw new Exception("Pokemon not found");
             return pokemon;
         }
-
+      // Method to retrieve a Pokémon by its name
         public async Task<Pokemon> GetPokemonByName(string name)
         {
             var pokemon = await _pokemonCollection.Find(p => p.Name == name).FirstOrDefaultAsync();
@@ -35,12 +36,13 @@ namespace pokedex.Services
                 throw new Exception("Pokemon not found");
             return pokemon;
         }
-
+             // Method to add a new Pokémon to the collection
         public async Task<Pokemon> AddPokemon(Pokemon newPokemon)
         {
             await _pokemonCollection.InsertOneAsync(newPokemon);
             return newPokemon;
         }
+         // Method to update an existing Pokémon by its ID
 
         public async Task<Pokemon> UpdatePokemon(string id, Pokemon updatedPokemon)
         {
@@ -49,6 +51,7 @@ namespace pokedex.Services
                 throw new Exception("Pokemon not found");
             return updatedPokemon;
         }
+        // Method to delete a Pokémon by its ID
 
         public async Task<bool> DeletePokemon(string id)
         {
